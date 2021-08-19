ebaec6a18a6f0e5f5db23a56e84805e1c320a28c
Daniel Leal <dlleal@uc.cl>
2021-08-19 10:57:46 -0400

    fix: remove unnecessary exports and leave only a named export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 import Fintoc from './lib/core';
 
-// Type checking
-export default Fintoc;
 export { Fintoc };
-
-// Module exports
-module.exports = Fintoc;
-
-// Allow use with the TypeScript compiler without `esModuleInterop`
-module.exports.default = Fintoc;
-
-// Expose constructor as named property
-module.exports.Fintoc = Fintoc;
