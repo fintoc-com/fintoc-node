8ddb49f9c83ce3904cf742e2aa48ea2ce001e4ca
Daniel Leal <dlleal@uc.cl>
2021-12-09 17:15:23 -0300

    style: change mapping to snake case for consistency purposes

--- src/lib/resources/movement.ts
+++ src/lib/resources/movement.ts
@@ -2,7 +2,7 @@ import { ResourceMixin } from '../mixins/resourceMixin';
 
 export class Movement extends ResourceMixin<Movement> {
   static mappings = {
-    recipient_account: 'transferAccount',
-    sernder_account: 'transferAccount',
+    recipient_account: 'transfer_account',
+    sernder_account: 'transfer_account',
   }
 }
