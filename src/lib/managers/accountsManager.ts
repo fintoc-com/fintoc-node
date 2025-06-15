7b4ec3e5e2c207109c86174eb20b08004f032d2c
Lukas Zorich <lukas.zorich@gmail.com>
2025-06-15 14:22:42 -0600

    refactor: change SDK to use functional style for calling endpoints

--- src/lib/managers/accountsManager.ts
+++ src/lib/managers/accountsManager.ts
@@ -1,7 +1,17 @@
+import { Client } from '../client';
 import { ManagerMixin } from '../mixins';
 import { Account } from '../resources/account';
+
+import { MovementsManager } from './movementsManager';
 
 export class AccountsManager extends ManagerMixin<Account> {
   static resource = 'account';
   static methods = ['all', 'get'];
+
+  movements: MovementsManager;
+
+  constructor(path: string, client: Client) {
+    super(path, client);
+    this.movements = new MovementsManager('/accounts/{account_id}/movements', client);
+  }
 }
