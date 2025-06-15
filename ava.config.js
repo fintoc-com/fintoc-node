bce67bff894afe466ea0cf33ed91ca7538cd3587
Lukas Zorich <lukas.zorich@gmail.com>
2025-06-14 19:07:58 -0600

    chore: update dependencies

--- ava.config.js
+++ ava.config.js
@@ -1,10 +1,11 @@
-export default {
+module.exports = {
   failFast: true,
   timeout: '60s',
   typescript: {
     rewritePaths: {
       'src/': 'build/main/',
     },
+    compile: false,
   },
   files: [
     '!build/module/**',
