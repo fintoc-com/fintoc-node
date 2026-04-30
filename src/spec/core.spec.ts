d7467e6cb439b38beff1b4d840b15f34edb08812
Francisca Otero <48388881+FranciscaOtero@users.noreply.github.com>
2026-04-30 14:43:14 -0400

    feat: expose userAgent option in Fintoc constructor (#89)

--- src/spec/core.spec.ts
+++ src/spec/core.spec.ts
@@ -9,3 +9,17 @@ test('"Fintoc" object creation', (t) => {
   t.assert(fintoc.links instanceof LinksManager);
   t.assert(fintoc.webhookEndpoints instanceof WebhookEndpointsManager);
 });
+
+test('"Fintoc" object creation with custom userAgent', (t) => {
+  const apiKey = 'super_secret_api_key';
+  const fintoc = new Fintoc(apiKey, undefined, { userAgent: 'fintoc-cli/0.1.0' });
+  t.assert(fintoc.links instanceof LinksManager);
+  t.assert(fintoc.webhookEndpoints instanceof WebhookEndpointsManager);
+  t.is((fintoc.links as any)._client.userAgent, 'fintoc-cli/0.1.0');
+});
+
+test('"Fintoc" object creation uses default userAgent when not provided', (t) => {
+  const apiKey = 'super_secret_api_key';
+  const fintoc = new Fintoc(apiKey);
+  t.true((fintoc.links as any)._client.userAgent.startsWith('fintoc-node/'));
+});
