306e75776db838a16c0ec4ad3d4c2e5c5736896d
tlarrain <tomas.larrain@fintoc.com>
2026-05-15 12:50:08 -0400

    feat: add webhook test endpoint

--- src/lib/managers/webhookEndpointsManager.ts
+++ src/lib/managers/webhookEndpointsManager.ts
@@ -1,7 +1,16 @@
 import { ManagerMixin } from '../mixins';
 import { WebhookEndpoint } from '../resources/webhookEndpoint';
+import { WebhookEvent } from '../resources/webhookEvent';
+import { canRaiseHTTPError, objetize } from '../utils';
 
 export class WebhookEndpointsManager extends ManagerMixin<WebhookEndpoint> {
   static resource = 'webhook_endpoint';
-  static methods = ['list', 'get', 'create', 'update', 'delete'];
+  static methods = ['list', 'get', 'create', 'update', 'delete', 'test'];
+
+  @canRaiseHTTPError
+  async test(webhookEndpointId: string, args: { type: string }): Promise<WebhookEvent> {
+    const path = `${this.buildPath()}/${webhookEndpointId}/test`;
+    const data = await this._client.request({ path, method: 'post', json: args });
+    return objetize(WebhookEvent, this._client, data);
+  }
 }
---
+++ src/spec/managers/webhookEndpointsManager.spec.ts
@@ -0,0 +1,40 @@
+import test from 'ava';
+
+import { Client } from '../../lib/client';
+import { WebhookEndpointsManager } from '../../lib/managers';
+import { WebhookEvent } from '../../lib/resources/webhookEvent';
+import { mockAxios, restore } from '../mocks/initializers';
+
+test.before((t) => {
+  const ctx: any = t.context;
+  ctx.axiosMock = mockAxios();
+});
+
+test.after((t) => {
+  const ctx: any = t.context;
+  restore(ctx.axiosMock);
+});
+
+test.beforeEach((t) => {
+  const ctx: any = t.context;
+
+  ctx.client = new Client({
+    baseUrl: 'https://test.com',
+    apiKey: 'super_secret_api_key',
+    userAgent: 'fintoc-node/test',
+  });
+  ctx.path = '/v1/webhook_endpoints';
+});
+
+test('"WebhookEndpointsManager.test" returns a WebhookEvent instance', async (t) => {
+  const ctx: any = t.context;
+  const manager = new WebhookEndpointsManager(ctx.path, ctx.client);
+
+  const event = await manager.test('we_123', { type: 'payment_intent.succeeded' });
+
+  t.true(event instanceof WebhookEvent);
+  t.is(event.id, 'we_123');
+  t.is(event.method, 'post');
+  t.is(event.url, 'v1/webhook_endpoints/we_123/test');
+  t.is(event.json.type, 'payment_intent.succeeded');
+});
