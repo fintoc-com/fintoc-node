f4337445d27804815ebbb6b4b1fcce04f88d3a16
mamunozcar <m.munozcarr@gmail.com>
2026-05-19 12:11:36 -0400

    fixup! feat: add support for disputes endpoints

--- src/lib/resources/dispute.ts
+++ src/lib/resources/dispute.ts
@@ -1,3 +1,7 @@
 import { ResourceMixin } from '../mixins/resourceMixin';
 
-export class Dispute extends ResourceMixin<Dispute> { }
+export class Dispute extends ResourceMixin<Dispute> {
+  static mappings = {
+    documents: 'dispute_document',
+  }
+}
